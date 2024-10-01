27ff46084ba63542ec9d2a419c341da33831abeb
SantiagoVallejo1 <santtyvallejo6@gmail.com>
2024-10-01 14:53:30 -0500

    first commit

---
+++ app/Http/Controllers/ControladorUsuarios.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ControladorUsuarios extends Controller
+{
+    public function control(){
+        return 'Hello World';
+    }
+}
